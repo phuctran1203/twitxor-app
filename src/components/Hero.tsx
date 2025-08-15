511db0a3fd0cbcb32142a0f8e18a7e2b61ffcc18
Phuc Tran TMPlate <phuctran12032004@gmail.com>
2025-08-15 18:36:22 +0700

    update little ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,10 +10,10 @@ export default function Hero() {
         ease={80}
         refresh
       />
-      <h1 className="relative mx-0 max-w-[43.5rem] pt-5 md:mx-auto md:px-4 md:py-2 text-balance text-left font-semibold tracking-tighter md:text-center text-5xl sm:text-7xl md:text-7xl lg:text-7xl">
+      <h1 className="relative mx-0 max-w-[43.5rem] pt-5 md:mx-auto px-4 md:py-2 text-balance text-center font-semibold tracking-tighter md:text-center text-5xl sm:text-7xl md:text-7xl lg:text-7xl">
         Everything Is A Gift
       </h1>
-      <p className="max-w-xl text-balance text-left text-base tracking-tight text-black dark:font-medium dark:text-white md:text-center md:text-lg ">
+      <p className="px-2 max-w-xl text-balance text-center text-base tracking-tight text-black dark:font-medium dark:text-white md:text-center md:text-lg ">
         I'm so bored, then I made a website just for sharing some things that
         you are looking for
       </p>
